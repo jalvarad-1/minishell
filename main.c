0b54e52c7c96fa0502fda24ab4aa13466c9cbaec
Roberto Rodriguez garcia <robrodri@c2r19s4.42madrid.com>
2021-10-20 17:06:54 +0200

    colors

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 # include "minishell.h"
 
+t_global g_common;
+
 int main (int argc, char **argv, char **env)
 {
 	ft_putstr("\nMinishell:");
+
 	while (1)
 		;
 }
--- minishell.h
+++ minishell.h
@@ -1,6 +1,13 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define CYAN "\033[0;36m"
+# define RESET "\033[0m"
+# define YELLOW "\033[0;33m"
 # include "./libft/includes/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
 # include <stdio.h>
 # include <sys/wait.h>
 # include <sys/types.h>
