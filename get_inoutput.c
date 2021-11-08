a608d91688d75d6dd3196a7198979e07eccb6949
Jose Alvarado cevallos <jalvarad@c1r1s6.42madrid.com>
2021-11-08 17:58:21 +0100

    get_inout

--- get_inoutput.c
+++ get_inoutput.c
@@ -1,4 +1,40 @@
 #include "minishell.h"
+int redirection_counter(char **token, char operator)
+{
+	int i;
+	int j;
+	int redirections;
+
+	i = 0;
+	redirections = 0;
+	while (token && token[i])
+	{
+		j = 0;
+		while (token[i][j])
+		{
+			if (token[i][j] == '\'' || token[i][j] == '"')
+			{
+				if (token[i][j] == '\'')
+				{
+					j++;
+					while(token[i][j] && token[i][j] != '\'')
+						j++;
+				}
+				else if (token[i][j] == '"')
+				{
+					j++;
+					while (token[i][j] && token[i][j] != '"')
+						j++;
+				}
+			}
+			if (token[i][j] == operator)
+				redirections++;
+			j++;
+		}
+		i++;
+	}
+	return (redirections);
+}
 
 char **ft_get_inputs(char ***token)
 {
@@ -7,12 +43,22 @@ char **ft_get_inputs(char ***token)
 	int i;
 	int j;
 
+	i = redirection_counter(*token, '<');
+	if (!i)
+		return (NULL);
+	fds = (char **)malloc(sizeof(char *) * (i + 1));
+	if (!fds)
+		return (NULL);
 	i = 0;
-	while(token[0][i][j])
+	while(token[0][i])
 	{
 		j = 0;
-		
-		
+		while (token[0][i][j])
+		{
+
+			j++;
+		}
+		i++;
 	}
 	return (fds);
 }
