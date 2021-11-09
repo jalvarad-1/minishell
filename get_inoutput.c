ce3efbfb4c746377be584369afb6f305a0a44745
Jose Alvarado cevallos <jalvarad@c2r18s2.42madrid.com>
2021-11-09 18:02:47 +0100

    haciendo la recogida de redirecciones

--- get_inoutput.c
+++ get_inoutput.c
@@ -27,7 +27,7 @@ int redirection_counter(char **token, char operator)
 						j++;
 				}
 			}
-			if (token[i][j] == operator)
+			if (token[i][j] == operator && token[i][j + 1] != operator)
 				redirections++;
 			j++;
 		}
@@ -42,23 +42,36 @@ char **ft_get_inputs(char ***token)
 	char **fds;
 	int i;
 	int j;
+	int b;
 
-	i = redirection_counter(*token, '<');
+	i = redirection_counter(*token, '<'); /// contador de redirecciones;
 	if (!i)
 		return (NULL);
 	fds = (char **)malloc(sizeof(char *) * (i + 1));
 	if (!fds)
 		return (NULL);
-	i = 0;
 	while(token[0][i])
 	{
 		j = 0;
 		while (token[0][i][j])
 		{
-
+			if (token[0][i][j] == '<' && token[0][i][j + 1] != '<')
+			{
+				//if (token[0][i][j + 1])
+				//	fds[b] = ;
+			}
+			else if(token[0][i][j] == '<' && token[0][i][j + 1] == '<')
+				j++;
 			j++;
 		}
 		i++;
 	}
+	new_token = remove_ops_files(*token);
 	return (fds);
 }
+/*  remove_ops_files(char **token):
+	Esta función se encarga de remover tanto los operadores , 
+	como los archivos despues a este y por lo tanto a modifica
+	Ya que es complejo determinar las dimensiones de la "new_token" 
+	lo mas sencillo es crear un nuevo string completo con todo, 
+	excepto los operadores y el string singuiente a los operadores. */
