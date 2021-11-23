3e816305e9077d97922943c247405b6dfe6c0d2c
Roberto Rodriguez garcia <robrodri@c1r1s3.42madrid.com>
2021-11-23 12:50:12 +0100

    Seg fault from $ expand solved

--- parser_2.c
+++ parser_2.c
@@ -103,6 +103,7 @@ static void	ft_trim_quotes(char **str)
 static void	ft_expand(char **token, char **env)
 {
 	ft_dollar_detect(token, env);
+	// $$ te da el numero de proceso de bash. Deberiamos dar el ps de minishell?
 	ft_trim_quotes(token);
 }
 
