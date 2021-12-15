84b53d39fb806ae1ce0db7a1b8e48ff927698283
Roberto Rodriguez garcia <robrodri@c3r1s3.42madrid.com>
2021-12-15 16:06:24 +0100

    done

--- parser_2.c
+++ parser_2.c
@@ -59,31 +59,23 @@ void	ft_expand(char **token, char **env, int flag)
 	ft_trim_quotes(token, flag);
 }
 
-int	get_command_table(char *str, char **env, t_cmds **table)
+static int	dismember(char **cmd, char **env, t_cmds **table)
 {
-	char	**cmd;
-	char	**token;
 	int		i;
 	t_fds	*ins;
 	t_fds	*outs;
+	char	**token;
 
 	i = 0;
-	ins = NULL;
-	outs = NULL;
-	if (!rev_vertial_bars(str))
-		return (0);
-	cmd = ft_mod_split(str, '|');
-	if (!unquoted_marks(cmd))
-	{
-		free_matrix(cmd);
-		return (0);
-	}
+	token = NULL;
 	while (cmd[i])
 	{
 		token = ft_mod_split(cmd[i], ' ');
 		if (!operator_identifier(token))
 		{
 			free(cmd);
+			free_matrix(token);
+			ft_free_table(table);
 			return (0);
 		}
 		ins = ft_get_inputs(&token, '<', env);
@@ -92,6 +84,23 @@ int	get_command_table(char *str, char **env, t_cmds **table)
 		save_cmd(table, token, ins, outs);
 		i++;
 	}
+	return (1);
+}
+
+int	get_command_table(char *str, char **env, t_cmds **table)
+{
+	char	**cmd;
+	char	**token;
+
+	if (!rev_vertial_bars(str))
+		return (0);
+	cmd = ft_mod_split(str, '|');
+	if (!unquoted_marks(cmd))
+	{
+		free_matrix(cmd);
+		return (0);
+	}
+	dismember(cmd, env, table);
 	free_matrix(cmd);
 	return (1);
 }
